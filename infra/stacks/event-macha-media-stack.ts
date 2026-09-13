e4b40482ec33a1d714f9d31c3ca404e1e522442e
chaitanya <chaitanyagupta.gamini@gmail.com>
2026-09-13 19:54:58 +0530

    feat: add EventMachaMediaStack with S3 buckets for media storage

--- infra/stacks/event-macha-media-stack.ts
+++ infra/stacks/event-macha-media-stack.ts
@@ -9,6 +9,7 @@ export interface EventMachaMediaStackProps extends StackProps {
 
 export class EventMachaMediaStack extends Stack {
   public readonly mediaBucket: s3.Bucket;
+  public readonly posemachaMediaBucket: s3.Bucket;
 
   constructor(scope: Construct, id: string, props: EventMachaMediaStackProps) {
     super(scope, id, props);
@@ -18,6 +19,20 @@ export class EventMachaMediaStack extends Stack {
     // Create the Media S3 Bucket with public access enabled
     this.mediaBucket = new s3.Bucket(this, 'MediaBucket', {
       bucketName: `eventmacha-${appConfig.environment}-media`,
+      publicReadAccess: true,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }),
+      removalPolicy: appConfig.environment === 'prod' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
+      autoDeleteObjects: false,
+    });
+
+    // Create the Posemacha Media S3 Bucket with public access enabled
+    this.posemachaMediaBucket = new s3.Bucket(this, 'PosemachaMediaBucket', {
+      bucketName: `posemacha-${appConfig.environment}-media`,
       publicReadAccess: true,
       blockPublicAccess: new s3.BlockPublicAccess({
         blockPublicAcls: false,
@@ -40,6 +55,18 @@ export class EventMachaMediaStack extends Stack {
       value: this.mediaBucket.bucketRegionalDomainName,
       description: 'The domain name of the Event Macha Media S3 bucket',
       exportName: `EventMacha-${appConfig.environment}-MediaBucketDomainName`,
+    });
+
+    new CfnOutput(this, 'PosemachaMediaBucketName', {
+      value: this.posemachaMediaBucket.bucketName,
+      description: 'The name of the Posemacha Media S3 bucket',
+      exportName: `Posemacha-${appConfig.environment}-MediaBucketName`,
+    });
+
+    new CfnOutput(this, 'PosemachaMediaBucketDomainName', {
+      value: this.posemachaMediaBucket.bucketRegionalDomainName,
+      description: 'The domain name of the Posemacha Media S3 bucket',
+      exportName: `Posemacha-${appConfig.environment}-MediaBucketDomainName`,
     });
   }
 }
